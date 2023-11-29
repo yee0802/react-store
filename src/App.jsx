d4f1d1ff8bcc7f38521ec8c863fc2ef7a9299ac6
Kye <NovusAa12@proton.me>
2023-11-29 19:32:05 +0000

    add CartContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
+import { createContext, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/index";
 import "./App.css";
 
+const CartContext = createContext();
+
 function App() {
+  const [cart, setCart] = useState("");
+
   return (
     <>
       <div className="app-container">
-        <Header />
+        <CartContext.Provider value={{ cart, setCart }}>
+          <Header />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </CartContext.Provider>
       </div>
     </>
   );
 }
 
-export default App;
+export { App, CartContext };
