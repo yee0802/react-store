72c852cea3bf128e30b8d0c2dbc8a5774bb6534a
Kye <NovusAa12@proton.me>
2023-11-30 23:36:51 +0000

    update Home & add Reviews component

---
+++ src/pages/Home/components/Reviews/Reviews.jsx
@@ -0,0 +1,9 @@
+import ReviewsList from "./ReviewsList";
+
+export default function Reviews() {
+  return (
+    <article className="reviews">
+      <ReviewsList />
+    </article>
+  );
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,29 @@
 import { Link } from "react-router-dom";
+import "./index.css";
+import Reviews from "./components/Reviews/Reviews";
 
 export default function Home() {
   return (
-    <main className="home container">
-      <article className="home__welcome">
-        <h2>Welcome</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis hic
-          doloribus voluptatibus repellat, error esse libero maiores commodi! Ex
-          est odio, rem harum aperiam illum magnam necessitatibus saepe a
-          consectetur.
-        </p>
-        <button>
-          <Link to="/products"> View All Products</Link>
-        </button>
-      </article>
+    <main className="container">
+      <section id="home">
+        <article className="home__welcome">
+          <h2>Welcome To The Store</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <Link to="/products">
+            <button className="view-all-products">View All Products</button>
+          </Link>
+        </article>
+
+        <Reviews />
+      </section>
     </main>
   );
 }
