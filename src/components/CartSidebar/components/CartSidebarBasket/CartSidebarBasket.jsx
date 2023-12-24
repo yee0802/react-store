8896487dc7a357caea815c47d5709f347a053bce
Kye <NovusAa12@proton.me>
2023-12-24 12:23:50 +0000

    update View Cart button

--- src/components/CartSidebar/components/CartSidebarBasket/CartSidebarBasket.jsx
+++ src/components/CartSidebar/components/CartSidebarBasket/CartSidebarBasket.jsx
@@ -6,7 +6,18 @@ export default function CartSidebarBasket() {
     <section className="cart-sidebar__basket">
       <CartTotal />
       <Link to="/cart">
-        <button className="view-cart-btn">View Cart</button>
+        <button
+          className="view-cart-btn"
+          onClick={() => {
+            const cartBtn = document.getElementById("cart-btn");
+            const sidebar = document.getElementById("cart-sidebar");
+
+            cartBtn.classList.remove("active");
+            sidebar.classList.remove("active");
+          }}
+        >
+          View Cart
+        </button>
       </Link>
       <button className="checkout-btn">Checkout</button>
     </section>
