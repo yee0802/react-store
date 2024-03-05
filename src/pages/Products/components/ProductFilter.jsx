da88f6e0a3e69f9668ff138a28ee415b299be532
Kye <NovusAa12@proton.me>
2024-03-05 17:27:24 +0000

    update ProductFilter to filter by category using server sided filtering

--- src/pages/Products/components/ProductFilter.jsx
+++ src/pages/Products/components/ProductFilter.jsx
@@ -1,41 +1,66 @@
+import { useEffect, useState } from "react";
+import api from "../../../api";
+
 export default function ProductFilter(props) {
-  const { products, setProducts, defaultProducts } = props;
+  const { setProducts, defaultProducts } = props;
 
-  const filter = () => {
-    const sortBy = document.getElementById("sort-by");
-    const option = sortBy.value;
+  const [categories, setCategories] = useState([]);
 
-    const sortByHighLow = [...products]
-      .sort((a, b) => a.price - b.price)
-      .reverse();
-
-    const sortByLowHigh = [...products].sort((a, b) => a.price - b.price);
-
-    switch (option) {
-      case "high-low":
-        setProducts(sortByHighLow);
-        break;
-      case "low-high":
-        setProducts(sortByLowHigh);
-        break;
-      case "default":
-        setProducts(defaultProducts);
-        break;
-      default:
-        break;
+  const fetchCategoryById = async (id) => {
+    try {
+      const res = await api.get(`/categories/${id}`);
+      setProducts(res.data.category.products);
+    } catch (e) {
+      if (err.response) {
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else {
+        console.log(`Error: ${err.message}`);
+      }
     }
   };
 
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const res = await api.get("/categories");
+        setCategories(res.data.categories);
+      } catch (e) {
+        if (err.response) {
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error: ${err.message}`);
+        }
+      }
+    };
+    fetchCategories();
+  }, []);
+
   return (
-    <section className="filter--container" onChange={() => filter()}>
-      <select name="filter" id="sort-by">
-        <option value="default" hidden defaultValue>
-          Sort By
-        </option>
-        <option value="default">Default</option>
-        <option value="high-low">Price: High-Low</option>
-        <option value="low-high">Price: Low-High</option>
-      </select>
+    <section className="filter-container">
+      {categories.map((category, idx) => (
+        <button
+          key={idx}
+          className="filter-category--btn"
+          onClick={(e) => {
+            const button = e.currentTarget;
+            const isSelected = button.classList.contains("selected");
+
+            if (!isSelected) {
+              button.classList.add("selected");
+              fetchCategoryById(category.id);
+            } else {
+              button.classList.remove("selected");
+              setProducts(defaultProducts);
+            }
+          }}
+        >
+          {category.name}
+        </button>
+      ))}
     </section>
   );
 }
