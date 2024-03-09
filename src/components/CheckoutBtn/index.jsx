3a1276e3eb6a8d39ec2a23e5f2589b4c6ef6e39b
Kye <NovusAa12@proton.me>
2024-03-09 15:29:22 +0000

    add checkout functionality to CheckoutBtn

--- src/components/CheckoutBtn/index.jsx
+++ src/components/CheckoutBtn/index.jsx
@@ -1,9 +1,33 @@
+import api from "../../api";
 import "./index.css";
 
-export default function CheckoutBtn() {
+export default function CheckoutBtn(props) {
+  const { cart } = props;
+
   return (
     <>
-      <button className="checkout-btn">Checkout</button>
+      <button
+        className="checkout-btn"
+        onClick={async () => {
+          const cartData = cart.map(({ id, quantity }) => ({ id, quantity }));
+
+          try {
+            const res = await api.post("/create-checkout-session", cartData);
+
+            window.location.href = res.data.url;
+          } catch (err) {
+            if (err.response) {
+              console.log(err.response.data);
+              console.log(err.response.status);
+              console.log(err.response.headers);
+            } else {
+              console.log(`Error: ${err.message}`);
+            }
+          }
+        }}
+      >
+        Checkout
+      </button>
     </>
   );
 }
