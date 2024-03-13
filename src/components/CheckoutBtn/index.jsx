6ad4dab32c75804b7254d6e9ae0a5ff261464f4d
Kye <NovusAa12@proton.me>
2024-03-13 18:16:02 +0000

    update CheckoutBtn component

--- src/components/CheckoutBtn/index.jsx
+++ src/components/CheckoutBtn/index.jsx
@@ -1,12 +1,26 @@
 import { useState } from "react";
 import api from "../../api";
+import useAuth from "../../hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
 import "./index.css";
 
-export default function CheckoutBtn(props) {
-  const { cart } = props;
+export default function CheckoutBtn({ cart }) {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { token } = useAuth();
   const [checkoutMessage, setCheckoutMessage] = useState("");
 
   const handleCheckout = async () => {
+    if (!token) {
+      const cartBtn = document.getElementById("cart-btn");
+      const sidebar = document.getElementById("cart-sidebar");
+
+      cartBtn.classList.remove("active");
+      sidebar.classList.remove("active");
+
+      return navigate("/login", { state: { from: location } });
+    }
+
     if (cart.length === 0) {
       setCheckoutMessage("Error: Cannot checkout with an empty cart");
       return;
