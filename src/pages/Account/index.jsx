0048841ac717c2c80f3a2b09d1c3f32d29b0768c
Kye <NovusAa12@proton.me>
2024-03-18 16:54:45 +0000

    add handleAccountTabChange to Account

--- src/pages/Account/index.jsx
+++ src/pages/Account/index.jsx
@@ -1,15 +1,42 @@
+import EditProfileForm from "./components/EditProfileForm";
 import Profile from "./components/Profile";
 import "./index.css";
 
 export default function Account() {
+  const handleAccountTabChange = (event) => {
+    const buttons = document.querySelectorAll(".account-page--nav-btn");
+    const tabs = document.querySelectorAll(".tab");
+
+    // Find the index of the clicked button
+    const index = Array.from(buttons).indexOf(event.currentTarget);
+
+    tabs.forEach((tab) => tab.classList.remove("tab--active"));
+    buttons.forEach((button) => button.classList.remove("tab--active"));
+
+    tabs[index].classList.add("tab--active");
+    buttons[index].classList.add("tab--active");
+  };
+
   return (
     <main className="account-page container">
       <section className="account-page--content">
         <nav className="account-page--nav">
-          <button className="account-page--nav-btn">Profile</button>
+          <button
+            className="account-page--nav-btn"
+            onClick={(e) => handleAccountTabChange(e)}
+          >
+            Profile
+          </button>
+          <button
+            className="account-page--nav-btn"
+            onClick={(e) => handleAccountTabChange(e)}
+          >
+            Edit Profile
+          </button>
         </nav>
 
         <Profile />
+        <EditProfileForm />
       </section>
     </main>
   );
